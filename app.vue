71b9a03baed489ccdd82e6baf8c1ff21e6bc1e2c
lacene karima <lacene.karima@gmail.com>
2024-12-10 18:50:21 +0100

    Ajout de la fonctionnalité de création de posts mais pas fonctionel encore+ le like ne s'incremente pas aussi

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="bg-black fixed w-[calc(100%+1px)] h-full">
     <NuxtPage />
 
-    <CreatePost
+    <CreatePoste
       :class="[
           {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
           {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
